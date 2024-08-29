3ee99cb9f7fc44c445c7d320bacb1d3045fea55b
PaulaMaina <kabura.mainap@gmail.com>
2024-08-29 13:39:23 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,4 +10,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks if a hashed password was formed from a  given password"""
-    return bcypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

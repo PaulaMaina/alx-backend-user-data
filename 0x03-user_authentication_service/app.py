d331f61a562730a8b93c930593ae93748a7428dc
PaulaMaina <kabura.mainap@gmail.com>
2024-09-11 08:44:20 +0000

    Logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
 from auth import Auth
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, redirect, request
 
 
 app = Flask(__name__)
@@ -37,6 +37,17 @@ def login() -> str:
     res = jsonify({"email": email, "message": "logged in"})
     res.set_cookie("session_id", session_id)
     return res
+
+
+@app.route("/sessions", methods["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """Logs a user out and deletes the session"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_by_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":

e602d89d1ba5e9bbfda1464118b7534072ebe064
PaulaMaina <kabura.mainap@gmail.com>
2024-09-11 08:45:44 +0000

    Logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,7 +43,7 @@ def login() -> str:
 def logout() -> str:
     """Logs a user out and deletes the session"""
     session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_by_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
